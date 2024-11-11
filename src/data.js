7d46b688d41792dc2b6caed5a87047045ffe6432
Krish-Raj-Mittal <krishrajmittal@gmail.com>
2024-11-11 12:26:13 +0530

    Project Completed on (11 November 2024) => Project {tour app using react}

---
+++ src/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        id:1,
+        name: "Agra",
+        info: "Agra is a city of india. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis autem, aperiam reprehenderit obcaecati at accusantium repudiandae necessitatibus nemo optio pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo fuga molestias odit aut magni? ",
+        image: "https://cdn.pixabay.com/photo/2016/05/08/15/23/india-1379273_960_720.jpg 1x, https://cdn.pixabay.com/photo/2016/05/08/15/23/india-1379273_1280.jpg",
+        price: "35,758"
+    },
+
+    {
+        id:2,
+        name: "Jaipur",
+        info: "Jaipur is a city of india. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis autem, aperiam reprehenderit obcaecati at accusantium repudiandae necessitatibus nemo optio pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo fuga molestias odit aut magni? ",
+        image: "https://theheritageart.com/wp-content/uploads/2022/11/hawa-mahal.jpg",
+        price: "85,560"
+    },
+
+    {
+        id:3,
+        name: "Kochi",
+        info: "Kochi is a city of india. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis autem, aperiam reprehenderit obcaecati at accusantium repudiandae necessitatibus nemo optio pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo fuga molestias odit aut magni? ",
+        image: "https://upload.wikimedia.org/wikipedia/commons/8/8f/Kochi_Skyline.jpg",
+        price: "60,668"
+    },
+
+    {
+        id:4,
+        name: "Varanasi",
+        info: "Varanasi is a city of india. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis autem, aperiam reprehenderit obcaecati at accusantium repudiandae necessitatibus nemo optio pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo fuga molestias odit aut magni? ",
+        image: "https://media.istockphoto.com/id/1164329797/photo/hindu-sadhu-sitting-on-a-boat-overlooking-varanasi-city-architecture-at-sunset.jpg?s=612x612&w=0&k=20&c=LbpIHRo7kGT7dbUr6b6UuD1d6P0yCaKZ2lbqo3TY988=",
+        price: "55,758"
+    },
+
+    {
+        id:5,
+        name: "Goa",
+        info: "Goa is a city of india. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis autem, aperiam reprehenderit obcaecati at accusantium repudiandae necessitatibus nemo optio pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo fuga molestias odit aut magni? ",
+        image: "https://images.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/08/16/Pictures/_dbca9ffa-a138-11e8-9345-8d51f8ed9678.jpg",
+        price: "45,758"
+    },
+
+    {
+        id:6,
+        name: "Delhi",
+        info: "Delhi is a city of india. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis autem, aperiam reprehenderit obcaecati at accusantium repudiandae necessitatibus nemo optio pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo fuga molestias odit aut magni? ",
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/2a/Delhi_fort.jpg",
+        price: "35,758"
+    },
+
+    {
+        id:7,
+        name: "Konark Sun Temple",
+        info: "Agra is a city of india. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis autem, aperiam reprehenderit obcaecati at accusantium repudiandae necessitatibus nemo optio pariatur! Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum nemo fuga molestias odit aut magni? ",
+        image: "https://d5mag.com/wp-content/uploads/2024/03/Cover-2-1920x1024.jpg",
+        price: "29,758"
+    }
+]
+
+export default data;
